f4c9e2cf3b7826c4b5c60860cb904203ea85c47b
PGreatness <ahnafhasan2000@gmail.com>
2023-04-16 15:44:18 -0400

    test

--- pa4_handout/pa4_template/src/main.cpp
+++ pa4_handout/pa4_template/src/main.cpp
@@ -60,7 +60,7 @@ std::shared_ptr<Shader> axisShader;       // shader for x, y, z axis
 // TODO: Add other context configurations
 std::shared_ptr<Shader> cubeShader;       // shader for cubes
 Cube cube;                               // default cube object
-struct Options cubeOptions;
+struct Cube::Options cubeOptions;
 cubeOptions = cube.getOptions();
 
 }  // namespace Context
