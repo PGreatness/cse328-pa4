762b5d18666443cdb85e5049f2a297bc5faf90a5
PGreatness <ahnafhasan2000@gmail.com>
2023-04-17 18:38:37 -0400

    test

--- pa4_handout/pa4_template/src/main.cpp
+++ pa4_handout/pa4_template/src/main.cpp
@@ -185,7 +185,7 @@ int main()
     // set lighting uniforms
     Context::cubeShader->setVec3("lightPos", Context::lightPos);
     Context::cubeShader->setVec3("viewPos", Context::camera.position);
-    Context::cubeShader->setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+    Context::cubeShader->setVec3("lightColor", 0.5f, 0.5f, 0.5f);
     // render loop
     glEnable(GL_DEPTH_TEST);
     glViewport(0, 0, Context::kWindowWidth, Context::kWindowHeight);
