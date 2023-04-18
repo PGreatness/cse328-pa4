024d20e2c5350cc6897b184407630dca1374487f
PGreatness <ahnafhasan2000@gmail.com>
2023-04-17 23:21:27 -0400

    add code forr octa

--- pa4_handout/pa4_template/include/shape/Octahedron.h
+++ pa4_handout/pa4_template/include/shape/Octahedron.h
@@ -6,9 +6,26 @@
 
 class Octahedron : public Shape
 {
+private:
+    struct Options
+    {
+        static const uint DEFAULT = 0;
+        static const uint SOLID = 0;
+        static const uint WIREFRAME = 1;
+        static const uint FLAT = 2;
+        static const uint SMOOTH = 4;
+    };
+
+    static const Options options;
+
 public:
     Octahedron()
     {
+        this->center = glm::vec3(DEFAULT_CENTER_X, DEFAULT_CENTER_Y, DEFAULT_CENTER_Z);
+        this->size = DEFAULT_SIZE;
+        this->color = glm::vec3(DEFAULT_COLOR_R, DEFAULT_COLOR_G, DEFAULT_COLOR_B);
+        this->oldColor = this->color;
+
         vertex.reserve(NUM_VERTICES);
 
         vertex.emplace_back(V3); vertex.emplace_back(V5); vertex.emplace_back(V1);
@@ -21,6 +38,99 @@ public:
         vertex.emplace_back(V6); vertex.emplace_back(V1); vertex.emplace_back(V4);
 
         // TODO
+    }
+
+    // getters
+    virtual GLint getNumVertices() const override { return NUM_VERTICES; }
+
+    virtual const glm::vec3 * getVertexData() const override {
+        glm::vec3 *vertexData = new glm::vec3[NUM_VERTICES];
+        for (int i = 0; i < NUM_VERTICES; i++) {
+            vertexData[i] = glm::vec3(this->vertex[i][0],
+                                        this->vertex[i][1],
+                                        this->vertex[i][2]);
+        }
+        return vertexData;
+    }
+
+    virtual const glm::vec3 * getNormalData() const override {
+        glm::vec3 *normalData = new glm::vec3[NUM_VERTICES];
+        for (int i = 0; i < NUM_VERTICES; i += 3) {
+            glm::vec3 v1 = glm::vec3(this->vertex[i][0],
+                                        this->vertex[i][1],
+                                        this->vertex[i][2]);
+            glm::vec3 v2 = glm::vec3(this->vertex[i + 1][0],
+                                        this->vertex[i + 1][1],
+                                        this->vertex[i + 1][2]);
+            glm::vec3 v3 = glm::vec3(this->vertex[i + 2][0],
+                                        this->vertex[i + 2][1],
+                                        this->vertex[i + 2][2]);
+            glm::vec3 normal = glm::normalize(glm::cross(v2 - v1, v3 - v1));
+            normalData[i] = normal;
+        }
+        return normalData;
+    }
+
+    glm::vec3 getCenter() const { return this->center; }
+    GLfloat getSize() const { return this->size; }
+    glm::vec3 getColor() const { return this->color; }
+    glm::vec3 getOldColor() const { return this->oldColor; }
+
+    // setters
+    void setCenter(glm::vec3 center) { this->translate(center - this->center); }
+    void setSize(GLfloat size) { this->scale(size / this->size); }
+    void setColor(glm::vec3 color) { this->oldColor = this->color; this->color = color; }
+
+    void translate(glm::vec3 translation) {
+        this->center += translation;
+        // center changed, so we need to update the vertex data
+        updateOctaLocation(translation);
+    }
+
+    void scale(GLfloat scale) {
+        this->size *= scale;
+        // size changed, so we need to update the vertex data
+        updateOctaSize(scale);
+    }
+
+    void rotate(GLfloat angle, glm::vec3 axis) {
+        updateOctaOrientation(angle, axis);
+    }
+
+    void render(GLuint octaArray, GLuint octaBuffer, GLuint shaderID) const {
+        initializeRender(&octaArray, &octaBuffer);
+
+        // set the color
+        GLint colorID = glGetUniformLocation(shaderID, "octaColor");
+        glUniform3f(colorID, this->color[0], this->color[1], this->color[2]);
+
+        // draw the octa
+        glDrawArrays(GL_TRIANGLES, 0, NUM_VERTICES);
+
+        // unbind the buffer
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    void render(GLuint octaArray, GLuint octaBuffer, GLuint shaderID, uint options) const {
+        initializeRender(&octaArray, &octaBuffer);
+
+        // set the color
+        GLint colorID = glGetUniformLocation(shaderID, "octaColor");
+        glUniform3f(colorID, this->color[0], this->color[1], this->color[2]);
+
+        // draw the octa
+        if (options & Options::WIREFRAME) {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            glDrawArrays(GL_LINE_STRIP, 0, NUM_VERTICES);
+        } else {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            glDrawArrays(GL_TRIANGLES, 0, NUM_VERTICES);
+        }
+
+        // unbind the buffer
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
 
 private:
@@ -37,6 +147,117 @@ private:
 
     // vertices organized into triangular facets
     std::vector<glm::vec3> vertex;
+
+    // the default octa is centered at the origin and has a side lengths of 1
+    static constexpr GLfloat DEFAULT_CENTER_X = 0.0f;
+    static constexpr GLfloat DEFAULT_CENTER_Y = 0.0f;
+    static constexpr GLfloat DEFAULT_CENTER_Z = 0.0f;
+    static constexpr GLfloat DEFAULT_SIZE = 1.0f;
+    // the deffault color is white
+    static constexpr GLfloat DEFAULT_COLOR_R = 1.0f;
+    static constexpr GLfloat DEFAULT_COLOR_G = 1.0f;
+    static constexpr GLfloat DEFAULT_COLOR_B = 1.0f;
+
+    glm::vec3 center;
+    GLfloat size;
+    glm::vec3 color;
+    glm::vec3 oldColor;
+
+    void updateOctaLocation(glm::vec3 translation) {
+        // update the vertex data
+        for (int i = 0; i < NUM_VERTICES; i++) {
+            vertex[i][0] += translation[0];
+            vertex[i][1] += translation[1];
+            vertex[i][2] += translation[2];
+        }
+    }
+
+    void updateOctaSize(GLfloat scale) {
+        // update the vertex data
+        for (int i = 0; i < NUM_VERTICES; i++) {
+            vertex[i][0] *= scale;
+            vertex[i][1] *= scale;
+            vertex[i][2] *= scale;
+        }
+    }
+
+    void rotateX(GLfloat angle) {
+        float angle_rad = glm::radians(angle);
+
+        glm::mat4 rotationMatrix = glm::mat4(1.0f);
+        rotationMatrix[1][1] = cos(angle_rad);
+        rotationMatrix[1][2] = -sin(angle_rad);
+        rotationMatrix[2][1] = sin(angle_rad);
+        rotationMatrix[2][2] = cos(angle_rad);
+
+        for (int i = 0; i < NUM_VERTICES; i++) {
+            glm::vec4 vertex4 = glm::vec4(vertex[i][0], vertex[i][1], vertex[i][2], 1.0f);
+            glm::vec4 rotatedVertex = rotationMatrix * vertex4;
+            vertex[i][0] = rotatedVertex[0];
+            vertex[i][1] = rotatedVertex[1];
+            vertex[i][2] = rotatedVertex[2];
+        }
+    }
+
+    void rotateY(GLfloat angle) {
+        float angle_rad = glm::radians(angle);
+
+        glm::mat4 rotationMatrix = glm::mat4(1.0f);
+        rotationMatrix[0][0] = cos(angle_rad);
+        rotationMatrix[0][2] = sin(angle_rad);
+        rotationMatrix[2][0] = -sin(angle_rad);
+        rotationMatrix[2][2] = cos(angle_rad);
+
+        for (int i = 0; i < NUM_VERTICES; i++) {
+            glm::vec4 vertex4 = glm::vec4(vertex[i][0], vertex[i][1], vertex[i][2], 1.0f);
+            glm::vec4 rotatedVertex = rotationMatrix * vertex4;
+            vertex[i][0] = rotatedVertex[0];
+            vertex[i][1] = rotatedVertex[1];
+            vertex[i][2] = rotatedVertex[2];
+        }
+    }
+
+    void rotateZ(GLfloat angle) {
+        float angle_rad = glm::radians(angle);
+
+        glm::mat4 rotationMatrix = glm::mat4(1.0f);
+        rotationMatrix[0][0] = cos(angle_rad);
+        rotationMatrix[0][1] = -sin(angle_rad);
+        rotationMatrix[1][0] = sin(angle_rad);
+        rotationMatrix[1][1] = cos(angle_rad);
+
+        for (int i = 0; i < NUM_VERTICES; i++) {
+            glm::vec4 vertex4 = glm::vec4(vertex[i][0], vertex[i][1], vertex[i][2], 1.0f);
+            glm::vec4 rotatedVertex = rotationMatrix * vertex4;
+            vertex[i][0] = rotatedVertex[0];
+            vertex[i][1] = rotatedVertex[1];
+            vertex[i][2] = rotatedVertex[2];
+        }
+    }
+
+    void updateOctaOrientation(GLfloat angle, glm::vec3 axis) {
+        if (axis[0] != 0) { rotateX(angle); }
+        if (axis[1] != 0) { rotateY(angle); }
+        if (axis[2] != 0) { rotateZ(angle); }
+    }
+
+    void initializeRender(GLuint * octaArray, GLuint * octaBuffer) const {
+        // create the vertex array object
+        glGenVertexArrays(1, octaArray);
+        glBindVertexArray(*octaArray);
+
+        // create the vertex buffer object
+        glGenBuffers(1, octaBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, *octaBuffer);
+
+        // get the vertex data
+        const glm::vec3 *dataStart = getVertexData();
+        glBufferData(GL_ARRAY_BUFFER, NUM_VERTICES * sizeof(glm::vec3), dataStart, GL_STATIC_DRAW);
+
+        // set up the vertex attributes
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    }
 };
 
 
---
+++ pa4_handout/pa4_template/src/shader/Octahedron/vert.glsl
@@ -0,0 +1,20 @@
+#version 410 core
+
+layout (location = 0) in vec3 aPos;
+
+out vec3 ourFragColor;
+out vec3 fragPos;
+out vec3 Normal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec3 octaColor;
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    Normal = vec3(transpose(inverse(model)) * vec4(aPos, 1.0));
+    fragPos = vec3(model * vec4(aPos, 1.0));
+    ourFragColor = tetraColor;
+}
