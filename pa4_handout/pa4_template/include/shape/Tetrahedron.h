7485c0638195ff0b9fdba7bccc54fdb63549ac65
PGreatness <ahnafhasan2000@gmail.com>
2023-04-19 14:38:05 -0400

    test

--- pa4_handout/pa4_template/include/shape/Tetrahedron.h
+++ pa4_handout/pa4_template/include/shape/Tetrahedron.h
@@ -139,7 +139,7 @@ public:
 
     // scale the tetrahedron
     void scale(float scaleFactor) {
-        this->size += 1.0f - scaleFactor;
+        this->size = 1.0f - scaleFactor;
         // size changed, so we need to update the vertex data
         std::cout << "scale factor: " << scaleFactor << std::endl;
         std::cout << "scale size: " << this->size << std::endl;
