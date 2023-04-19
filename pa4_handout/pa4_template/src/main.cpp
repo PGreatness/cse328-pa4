25b8d17140cfb437068b5d2d19f1a55de6305d7a
PGreatness <ahnafhasan2000@gmail.com>
2023-04-18 20:01:40 -0400

    test

--- pa4_handout/pa4_template/src/main.cpp
+++ pa4_handout/pa4_template/src/main.cpp
@@ -297,16 +297,16 @@ void cursorPosCallback(GLFWwindow * window, double xpos, double ypos)
 
     // make the mouse coordinates from the window coordinates
     // into the coordinates of the axis
-    glm::vec4 mouseCoords = glm::vec4(xpos, Context::kWindowHeight - ypos, 0.0f, 1.0f);
-    glm::mat4 projectionMatrix = glm::perspective(glm::radians(Context::camera.zoom),
-                                                 static_cast<GLfloat>(Context::kWindowWidth) /
-                                                 static_cast<GLfloat>(Context::kWindowHeight),
-                                                 0.01f,
-                                                 100.0f);
-    glm::mat4 viewMatrix = Context::camera.getViewMatrix();
-    mouseCoords = projectionMatrix * viewMatrix * mouseCoords;
-
-    Context::mouseLocalPos = glm::vec3(mouseCoords);
+    glm::mat4 projection = glm::perspective(glm::radians(Context::camera.zoom),
+                                            static_cast<GLfloat>(Context::kWindowWidth) /
+                                            static_cast<GLfloat>(Context::kWindowHeight),
+                                            0.01f,
+                                            100.0f);
+    glm::mat4 view = Context::camera.getViewMatrix();
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 mvp = projection * view * model;
+    glm::vec4 localPos = glm::inverse(mvp) * glm::vec4(xpos, ypos, 0.0f, 1.0f);
+    Context::mouseLocalPos = glm::vec3(localPos);
 }
 
 
