f949a8e968e4942eb106c9fc0dfda10c07096200
PGreatness <ahnafhasan2000@gmail.com>
2023-04-18 20:21:18 -0400

    test

--- pa4_handout/pa4_template/src/main.cpp
+++ pa4_handout/pa4_template/src/main.cpp
@@ -308,7 +308,7 @@ void cursorPosCallback(GLFWwindow * window, double xpos, double ypos)
     glm::vec4 localPos = glm::inverse(mvp) * glm::vec4(xpos, Context::kWindowHeight - ypos, 0.0f, 1.0f);
     glm::vec4 localOffset = glm::inverse(mvp) * glm::vec4(Context::kWindowWidth, Context::kWindowHeight, 0.0f, 1.0f);
     localOffset = (localOffset / 2.0f) / localOffset.w;
-    Context::mouseLocalPos = (glm::vec3(localOffset)) - (glm::vec3(localPos) / localPos.w);
+    Context::mouseLocalPos = (glm::vec3(localPos) / localPos.w) - (glm::vec3(localOffset));
 }
 
 
