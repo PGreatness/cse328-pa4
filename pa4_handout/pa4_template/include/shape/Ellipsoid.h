ddc3c75bf3a50a62cb0ab554c14be7d9a7ff7934
PGreatness <ahnafhasan2000@gmail.com>
2023-04-21 02:45:48 -0400

    test

--- pa4_handout/pa4_template/include/shape/Ellipsoid.h
+++ pa4_handout/pa4_template/include/shape/Ellipsoid.h
@@ -170,11 +170,13 @@ public:
     {
         std::cout << "subdividing ellipsoid" << std::endl;
         std::cout << "INIT_NUM_VERTICES: " << INIT_NUM_VERTICES << std::endl;
+        std::cout << "size: " << this->getNumVertices() << std::endl;
         std::cout << "this->subdivisionLevel: " << this->subdivisionLevel << std::endl;
         std::cout << "INIT_NUM_VERTICES * 3 * (this->subdivisionLevel + 1) before: " << INIT_NUM_VERTICES * 3 * (this->subdivisionLevel + 1) << std::endl;
         this->subdivision();
         this->subdivisionLevel++;
         std::cout << "INIT_NUM_VERTICES: " << INIT_NUM_VERTICES << std::endl;
+        std::cout << "size: " << this->getNumVertices() << std::endl;
         std::cout << "INIT_NUM_VERTICES * 3 * (this->subdivisionLevel + 1) after: " << INIT_NUM_VERTICES * 3 * (this->subdivisionLevel + 1) << std::endl;
     }
 
