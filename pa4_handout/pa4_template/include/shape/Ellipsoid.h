5767b5d064d753eb69d66eac8908be2a1c173a94
PGreatness <ahnafhasan2000@gmail.com>
2023-04-21 03:08:18 -0400

    test

--- pa4_handout/pa4_template/include/shape/Ellipsoid.h
+++ pa4_handout/pa4_template/include/shape/Ellipsoid.h
@@ -422,9 +422,9 @@ private:
         (*v12)[2] = ((*v1)[2] + (*v2)[2]) / 2;
 
         // scale it back to the ellipsoid
-        (*v12)[0] = (*v12)[0] * this->xAxisLength;
-        (*v12)[1] = (*v12)[1] * this->yAxisLength;
-        (*v12)[2] = (*v12)[2] * this->zAxisLength;
+        (*v12)[0] = (*v12)[0] * (this->xAxisLength / (*v12)[0]);
+        (*v12)[1] = (*v12)[1] * (this->yAxisLength / (*v12)[1]);
+        (*v12)[2] = (*v12)[2] * (this->zAxisLength / (*v12)[2]);
     }
 };
 
