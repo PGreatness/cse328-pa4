bc308dda47b1f79f1b4e88b316da370db7741312
PGreatness <ahnafhasan2000@gmail.com>
2023-04-22 17:04:24 -0400

    test

--- pa4_handout/pa4_template/src/main.cpp
+++ pa4_handout/pa4_template/src/main.cpp
@@ -64,7 +64,7 @@ namespace Transformations
         glm::vec3 ROTATION = glm::vec3(0.0f, 0.0f, 0.0f);
         glm::vec3 SCALE = glm::vec3(1.0f, 1.0f, 1.0f);
         glm::vec3 SHEAR = glm::vec3(0.0f, 0.0f, 0.0f);
-        glm::vec3 REFLECTION = std::array<glm::vec3, 2>{glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)};
+        std::array<glm::vec3, 2> REFLECTION = std::array<glm::vec3, 2>{glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)};
     };
 
     VALUES VALUES;
