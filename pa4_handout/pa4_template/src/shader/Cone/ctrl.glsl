9a7f1b210de97d26dc97239114e7b5045c02dc7f
PGreatness <ahnafhasan2000@gmail.com>
2023-04-23 00:44:48 -0400

    dnw

--- pa4_handout/pa4_template/src/shader/Cone/ctrl.glsl
+++ pa4_handout/pa4_template/src/shader/Cone/ctrl.glsl
@@ -1,38 +1,18 @@
-#version 410 core
+#version 410
 
-layout (vertices = 3) out;
+layout (vertices = 1) out;
 
-out vec3 normal[];
-out vec3 position[];
+void main()
+{
+    gl_TessLevelOuter[0] = 64;
+    gl_TessLevelOuter[1] = 64;
+    gl_TessLevelOuter[2] = 64;
+    gl_TessLevelOuter[3] = 64;
 
-uniform vec3 center;
-uniform float radius;
-uniform float height;
+    gl_TessLevelInner[0] = 64;
+    gl_TessLevelInner[1] = 64;
 
-const float PI = 3.14159265359;
-
-void main() {
-    // Calculate the positions of the vertices for the base of the cone
-    vec3 v0 = vec3(center.x, center.y, center.z);
-    for (int i = 0; i < 2; i++) {
-        float angle = 2.0 * PI * (float(i) / 2.0);
-        vec3 v = vec3(center.x + radius * cos(angle), center.y, center.z + radius * sin(angle));
-        position[i] = v;
-    }
-
-    // Calculate the position of the vertex for the top of the cone
-    position[2] = vec3(center.x, center.y + height, center.z);
-
-    // Calculate the normal vector for each vertex
-    for (int i = 0; i < 3; i++) {
-        normal[i] = normalize(cross(position[(i + 1) % 3] - position[i], position[(i + 2) % 3] - position[i]));
-    }
-
-    // Pass the positions and normals to the tessellation evaluation shader
-    gl_TessLevelOuter[0] = 3;
-    gl_TessLevelInner[0] = 1;
-
-    for(int i = 0; i < 3; i++){
-        gl_out[i].gl_Position = gl_in[i].gl_Position;
-    }
+    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 }
+
+
--- pa4_handout/pa4_template/src/shader/Cone/eval.glsl
+++ pa4_handout/pa4_template/src/shader/Cone/eval.glsl
@@ -2,7 +2,6 @@
 
 layout(triangles, equal_spacing, cw) in;
 
-in vec3 position[];
 
 out vec3 Normal;
 out vec3 fragPos;
@@ -10,25 +9,22 @@ out vec3 fragPos;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform vec3 center;
+uniform float radius;
+uniform float height;
+
+const float PI = 3.1415926535897932384626433832795;
 
 void main() {
     float u = gl_TessCoord.x;
     float v = gl_TessCoord.y;
 
-    vec3 A = mix(position[0], position[1], u);
-    vec3 B = mix(position[0], position[2], v);
+    // this should end up making a cone
+    vec3 pos = vec3(center.x + radius * cos(u * 2.0 * PI), center.y + radius * sin(u * 2.0 * PI), center.z + height * v);
 
-    float h = length(B - A);
-    vec3 P = mix(A, B, v / h);
-    vec3 T = normalize(B - A);
+    gl_Position = projection * view * model * vec4(pos, 1.0);
 
-    vec3 N = cross(T, vec3(0, 1, 0));
-    if (length(N) < 0.01) {
-        N = cross(T, vec3(1, 0, 0));
-    }
-    N = normalize(N);
+    Normal = normalize(pos - center);
+    fragPos = pos;
 
-    gl_Position = projection * view * model * vec4(P, 1);
-    fragPos = vec3(model * vec4(P, 1));
-    Normal = vec3(transpose(inverse(model)) * vec4(N, 0));
 }
