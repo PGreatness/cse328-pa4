75aef8d29ea41d953e060690b21411e83b95e4dc
PGreatness <ahnafhasan2000@gmail.com>
2023-04-23 00:55:18 -0400

    test

--- pa4_handout/pa4_template/src/shader/Cone/ctrl.glsl
+++ pa4_handout/pa4_template/src/shader/Cone/ctrl.glsl
@@ -3,13 +3,13 @@ layout (vertices = 3) out;
 
 void main()
 {
-    gl_TessLevelOuter[0] = 1;
-    gl_TessLevelOuter[1] = 1;
-    gl_TessLevelOuter[2] = 1;
-    gl_TessLevelOuter[3] = 1;
+    gl_TessLevelOuter[0] = 64;
+    gl_TessLevelOuter[1] = 64;
+    gl_TessLevelOuter[2] = 64;
+    gl_TessLevelOuter[3] = 64;
 
-    gl_TessLevelInner[0] = 1;
-    gl_TessLevelInner[1] = 1;
+    gl_TessLevelInner[0] = 64;
+    gl_TessLevelInner[1] = 64;
 
     gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 }
