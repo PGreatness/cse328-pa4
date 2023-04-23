2ee470dce6f05e6c05e9a825feab6539488b3db7
PGreatness <ahnafhasan2000@gmail.com>
2023-04-23 00:26:36 -0400

    cylinder does not have top or bottom

--- pa4_handout/pa4_template/src/shader/Cylinder/Bases/eval.glsl
+++ pa4_handout/pa4_template/src/shader/Cylinder/Bases/eval.glsl
@@ -1,26 +1,28 @@
 #version 410 core
-layout (triangles, equal_spacing, cw) in;
+layout(triangles, equal_spacing, cw) in;
 
-in vec3 position[];
+in vec3 Normal[];
+in vec3 fragPos[];
 
-out vec3 Normal;
-out vec3 fragPos;
-
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+out vec3 outColor;
 
 void main()
 {
-    vec3 p0 = position[0];
-    vec3 p1 = position[1];
-    vec3 p2 = position[2];
+    vec3 center = (fragPos[0] + fragPos[1] + fragPos[2]) / 3.0;
 
-    vec3 normal = normalize(cross(p1 - p0, p2 - p0));
-    Normal = normal;
+    vec3 v0 = fragPos[0] - center;
+    vec3 v1 = fragPos[1] - center;
+    vec3 v2 = fragPos[2] - center;
 
-    vec3 pos = (p0 + p1 + p2) / 3.0;
-    gl_Position = projection * view * model * vec4(pos, 1);
+    vec3 normal = cross(v1 - v0, v2 - v0);
+    normal = normalize(normal);
 
-    fragPos = vec3(model * vec4(pos, 1));
+    vec3 pos = vec3(fragPos[0].x, center.y, fragPos[0].z);
+    vec3 toCenter = center - pos;
+
+    float radius = length(toCenter);
+
+    outColor = vec3(1.0);
+
+    gl_Position = vec4(fragPos[0].x, center.y, fragPos[0].z, 1.0);
 }
