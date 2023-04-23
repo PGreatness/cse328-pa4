cbc5f4671b9b0f18ea3d9df072bdf77a885b9edd
PGreatness <ahnafhasan2000@gmail.com>
2023-04-23 03:06:51 -0400

    test

--- pa4_handout/pa4_template/include/shape/Torus.h
+++ pa4_handout/pa4_template/include/shape/Torus.h
@@ -409,7 +409,7 @@ private:
         glm::vec3 planeNormal = glm::normalize(glm::cross(planeA, planeB));
 
         // get the reflection matrix
-        glm::mat4 P = glm::outerProduct(glm::vec4(planeNormal, 0.0f), glm::mat4(planeNormal, 0.0f));
+        glm::mat4 P = glm::outerProduct(glm::vec4(planeNormal, 0.0f), glm::vec4(planeNormal, 0.0f));
         glm::mat4 R = glm::mat4(1.0f) - 2.0f * P;
 
         // multiply the reflection matrix with each vertex
